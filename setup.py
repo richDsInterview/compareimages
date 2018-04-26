c3e36eb87615679da2ab48f5650cdf2a1e5b68df
Richard Armstrong <richarms@ska.ac.za>
2018-04-26 06:47:13 +0200

    changed package name away from default

--- setup.py
+++ setup.py
@@ -10,13 +10,13 @@ with open('LICENSE') as f:
     license = f.read()
 
 setup(
-    name='template',
+    name='compareimages',
     version='0.1.0',
-    description='template project',
+    description='python modules to compare images in various ways',
     long_description=readme,
     author='Richard Armstrong',
     author_email='armstrong.richard@gmail.com',
-    url='https://github.com/',
+    url='https://github.com/richDsInterview/compareimages',
     license=license,
     packages=find_packages(exclude=('tests', 'docs'))
 )
